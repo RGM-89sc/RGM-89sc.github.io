70bd4cad596a8b8ff1cd97dd3392b7c80ef4f9a6
RGM-89sc <rgm89sc@gmail.com>
2018-12-09 10:37:19 +0800

    Site updated: 2018-12-09 10:37:17

--- js/autoClickAboutMe.js
+++ js/autoClickAboutMe.js
@@ -1,8 +1,8 @@
-(function(){
+(function () {
   // 自动点击about me
   var aboutMe = document.getElementsByClassName('about-me')[0];
 
-  var pageWidth = 
+  var pageWidth =
     window.innerWidth ? window.innerWidth : document.body.clientWidth;
   if (pageWidth >= 1200) {
     setTimeout(function () {
